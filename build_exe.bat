dc714dcd2d3d69aa391089f5de5535a0c6b5faf4
shihmin01ai <shihmin01@gmail.com>
2026-03-18 11:09:58 +0800

    build: Switch PyInstaller from onefile to onedir for faster startup

--- build_exe.bat
+++ build_exe.bat
@@ -11,7 +11,7 @@ echo 2. 正在開始建置 (這需要幾分鐘，請稍候)...
 :: --windowed: 執行時不顯示黑色的命令提示字元視窗
 :: --name: 設定檔案名稱
 :: --collect-all: 強制收集套件的所有中繼資料 (解決 MoviePy/imageio 報錯問題)
-python -m PyInstaller --onefile --windowed --name FastClip_程式 --collect-all moviepy --collect-all imageio --collect-all yt-dlp --collect-all pillow_heif main_gui.py
+python -m PyInstaller --onedir --windowed --name FastClip_程式 --collect-all moviepy --collect-all imageio --collect-all yt-dlp --collect-all pillow_heif main_gui.py
 
 echo.
 echo ========================================
